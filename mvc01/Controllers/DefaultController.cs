d0b35012ef13eaf18e6d59326634bc7e758c9bdb
tonykung <tonykung06@gmail.com>
2016-06-05 20:57:59 +0800

    initial commit

---
+++ mvc01/Controllers/DefaultController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc01.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "<a href='/Default/Index'>Hello!!!</a>";
+        }
+    }
+}
---
+++ mvc01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvc01.MvcApplication" Language="C#" %>
